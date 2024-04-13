b2f77a6f9553896bc373e281610e376cef78de22
Francis Lagares <francis.lagares@gmail.com>
2024-04-13 18:27:06 +0200

    feat: project scaffold init

---
+++ src/constants/icons.ts
@@ -0,0 +1,29 @@
+import bookmark from '@/constants/icons/bookmark.png';
+import eyeHide from '@/constants/icons/eye-hide.png';
+import eye from '@/constants/icons/eye.png';
+import home from '@/constants/icons/home.png';
+import leftArrow from '@/constants/icons/left-arrow.png';
+import logout from '@/constants/icons/logout.png';
+import menu from '@/constants/icons/menu.png';
+import play from '@/constants/icons/play.png';
+import plus from '@/constants/icons/plus.png';
+import profile from '@/constants/icons/profile.png';
+import rightArrow from '@/constants/icons/right-arrow.png';
+import search from '@/constants/icons/search.png';
+import upload from '@/constants/icons/upload.png';
+
+export default {
+  play,
+  bookmark,
+  home,
+  plus,
+  profile,
+  leftArrow,
+  menu,
+  search,
+  upload,
+  rightArrow,
+  logout,
+  eyeHide,
+  eye,
+};
