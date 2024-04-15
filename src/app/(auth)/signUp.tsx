8665800a3bb0625f340737831d53cb8eded0567b
Francis Lagares <francis.lagares@gmail.com>
2024-04-15 14:39:14 +0200

    feat: create signUp screen

---
+++ src/app/(auth)/signUp.tsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import { Link } from 'expo-router';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import CustomButton from '@/components/CustomButton';
+import FormField from '@/components/FormField';
+import { images } from '@/constants';
+
+const SignUp = () => {
+  const [form, setForm] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submitForm = () => {};
+
+  return (
+    <SafeAreaView className='h-full bg-primary '>
+      <ScrollView>
+        <View className='h- my-6 min-h-[85vh] w-full justify-center px-4'>
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='h-[35px] w-[135px] '
+          />
+          <Text className='text-semibold mt-10 font-psemibold text-2xl text-white'>
+            Sign up to Aora
+          </Text>
+
+          <FormField
+            title='Username'
+            value={form.username}
+            handleChangeText={e => setForm({ ...form, username: e })}
+            otherStyles='mt-10'
+          />
+
+          <FormField
+            title='Email'
+            value={form.email}
+            handleChangeText={e => setForm({ ...form, email: e })}
+            otherStyles='mt-7'
+            keyboardType='email-address'
+          />
+
+          <FormField
+            title='Password'
+            value={form.password}
+            handleChangeText={e => setForm({ ...form, password: e })}
+            otherStyles='mt-7'
+            keyboardType='email-address'
+          />
+
+          <CustomButton
+            title='Sign Up'
+            handlePress={submitForm}
+            containerStyles='mt-7'
+            isLoading={isSubmitting}
+          />
+
+          <View className='flex-row justify-center gap-2 pt-5'>
+            <Text className='font-pregular text-lg text-gray-100'>
+              Have an account already?
+            </Text>
+            <Link
+              href='/signIn'
+              className='font-psemibold text-lg text-secondary'
+            >
+              Sign In
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
